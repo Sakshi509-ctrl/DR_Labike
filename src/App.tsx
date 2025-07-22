a9a400120bfdbfe09b4fcc9fd9fb34fc18b5eb0f
Sakshi509-ctrl <sakshi.otusone@gmail.com>
2025-07-22 17:54:23 +0530

    franchise

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import ContactSidebar, { ContactSidebarProvider } from './components/ContactSidebar';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import TestimonialPage from './components/TestimonialPage';
 import BlogsPage from './components/BlogsPage';
 import BlogDetailPage from './components/BlogDetailPage';
@@ -10,7 +10,6 @@ import Home from './pages/Home';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
 import Sidebar from './components/Sidebar';
-import Hero from './components/Hero';
 import Franchise from './components/Franchise';
 import Internship from './components/internship';
 import MarketChart from './components/marketing';
@@ -31,9 +30,8 @@ function App() {
         <Route path="/blogs/:id" element={<BlogDetailPage />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact" element={<Contact />} />
-<Route path="/" element={<Hero />} />
         <Route path="/franchise" element={<Franchise />} />    
-            <Route path="/internship" element={<Internship/>}/>
+        <Route path="/internship" element={<Internship/>}/>
         <Route path='/marketing' element={<MarketChart/>}/>
         <Route path='/internshipform' element={<InternshipForm/>}/>
         <Route path='/roicalculator' element={<ROICalculator/>}/>
