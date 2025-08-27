cbbe54152c2b8c522fa57920e1aca606691b84e3
Sakshi509-ctrl <sakshi.otusone@gmail.com>
2025-08-27 17:41:10 +0530

    bcryptjs

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
+
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const sendEmail = require('../utils/sendEmail');
